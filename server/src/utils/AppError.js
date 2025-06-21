2cbffa222ff9f56cbe2a80f1cf58d36c0418a218
dxaginfo <142343166+dxaginfo@users.noreply.github.com>
2025-06-21 11:57:42 -0300

    Add AppError utility class

---
+++ server/src/utils/AppError.js
@@ -0,0 +1,20 @@
+/**
+ * Custom error class for API errors with status codes
+ * @extends Error
+ */
+class AppError extends Error {
+  /**
+   * Create a new AppError
+   * @param {string} message - Error message
+   * @param {number} statusCode - HTTP status code
+   */
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+    this.isOperational = true;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+module.exports = AppError;
